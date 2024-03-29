5c855a3f80dfeae948e6f9965f436040f75d7d13
JulieWustefeld <juliewustefeld@gmail.com>
2024-03-29 14:34:35 +0100

    solve ex 2.3

--- 02.control-structures/2.3/app.js
+++ 02.control-structures/2.3/app.js
@@ -1,3 +1,17 @@
-(() => {
-    // YOUR CODE HERE
-})();
+let i;
+let a = 100;
+
+console.log('Here are all the even numbers included between 1 and 100:');
+
+for (i=1;i<=a;i++) {
+    if(i%2==0) {
+        console.log(i);
+    };
+};
+
+while (i<a) {
+    if(i%2==0) {
+        console.log(i + ' ');
+    };
+    i++;
+};
