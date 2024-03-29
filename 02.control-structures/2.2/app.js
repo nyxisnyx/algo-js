8906d341f8f8820ccf71245c17e7ecc1805cbe4f
JulieWustefeld <juliewustefeld@gmail.com>
2024-03-29 14:07:30 +0100

    solve ex 2.2

--- 02.control-structures/2.2/app.js
+++ 02.control-structures/2.2/app.js
@@ -1,3 +1,11 @@
-(() => {
-    // YOUR CODE HERE
-})();
+let current = new Number (prompt("Please enter any number."););
+let min = new Number (prompt("Please enter a small number."););
+let max = new Number (prompt("Please enter a big number."););
+
+if (current > min && current < max) {
+    console.log(current);
+} 
+
+if (min > max) {
+    console.log('Oh dear. It seems to me that your cognitive function is severely impaired. I would strongly advise on medical attention. Your health is more important than testing silly little code on the internet.');
+}
