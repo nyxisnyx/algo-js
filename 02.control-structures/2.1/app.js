f4f0e5aac6e37d49acc89abc8d87fcbfac183b3f
JulieWustefeld <juliewustefeld@gmail.com>
2024-03-29 13:49:38 +0100

    Solve ex 2.1

--- 02.control-structures/2.1/app.js
+++ 02.control-structures/2.1/app.js
@@ -1,10 +1,9 @@
-(() => {
-    let age = prompt("Please enter your age.");
+
+    let age = new Number(prompt("Please enter your age."));
 
     if (age >= 18) {
         console.log('You are an adult.');
 }
-    else () {
+    else {
         console.log("You are a child.");
 }
-})();
