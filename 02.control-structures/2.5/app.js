fe255d8d021deb9a2f8342a97b089b41f9d3f1e8
JulieWustefeld <juliewustefeld@gmail.com>
2024-03-29 15:51:18 +0100

    completed the solve

--- 02.control-structures/2.5/app.js
+++ 02.control-structures/2.5/app.js
@@ -1,3 +1,7 @@
-(() => {
-    // YOUR CODE HERE
-})();
+/*Make a program that asks the favorite number of the user. If that number is anything other than 42 display "Are you sure?" and ask again. (This program should never end as long as the user didn't chose 42).*/
+
+let favNum = new Number prompt('What is your favourite number?');
+
+while (favNum =! 42) {
+    favNum = new Number prompt('Are you sure?')
+}
