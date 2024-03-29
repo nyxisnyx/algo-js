7db7e137925cd72be905f2929690c899c5be9a1f
JulieWustefeld <juliewustefeld@gmail.com>
2024-03-29 11:51:18 +0100

    add variables name, firstName and city then display them in console

--- 01.intro/1.2/app.js
+++ 01.intro/1.2/app.js
@@ -1,3 +1,7 @@
 (() => {
-    
+    let name = 'Cavalier';
+    let firstName = 'Nyx';
+    let city = 'Brussels';
+
+    console.log(`Your name is ${firstName} ${name} and you live in ${city}.`);
 })();
