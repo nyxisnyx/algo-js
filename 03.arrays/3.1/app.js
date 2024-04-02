3194d44c0dc56a177d42c0fd30cbff9b1eb4832b
Julie Wustefeld <Urubaean>
2024-04-02 10:41:55 +0200

    Solve ex 3.1

--- 03.arrays/3.1/app.js
+++ 03.arrays/3.1/app.js
@@ -1,3 +1,23 @@
-(() => {
-    // YOUR CODE HERE
-})();
+/*Write a program that will add all the elements of an array.
+
+Test it with the following arrays:
+
+[1, 2, 3, 4, 5] => 15
+[100, 101, 102] => 303*/
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [100, 101, 102];
+
+function sumArray(array) {
+    let sum = 0;
+    for (i=0; i<array.length;i++)  {
+        sum += array[i];
+        /*console.log(i);
+        console.log(array[i]);*/
+    };
+    console.log(sum);
+}
+
+sumArray(array1);
+sumArray(array2);
+
