f80ffa60ae5331875e9ff9087ecef6cb6fc78874
Julie Wustefeld <Urubaean>
2024-04-02 10:58:26 +0200

    solve ex 3.2

--- 03.arrays/3.2/app.js
+++ 03.arrays/3.2/app.js
@@ -1,3 +1,22 @@
-(() => {
-    // YOUR CODE HERE
-})();
+/*Write a program that will calculate the average value of a given array.
+
+Test it with the following arrays:
+
+[1, 2, 3, 4, 5] => 3
+[100, 101, 102] => 101*/
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [100, 101, 102];
+
+function avgArray(array) {
+    let sum = 0;
+    for (i=0; i<array.length;i++)  {
+        sum += array[i];
+        /*console.log(i);
+        console.log(array[i]);*/
+    };
+    console.log(sum / array.length);
+}
+
+avgArray(array1);
+avgArray(array2);
