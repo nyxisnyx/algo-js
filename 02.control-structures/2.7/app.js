773ae0014bc216e0a15f9779154afa2b7df2700f
Julie Wustefeld <Urubaean>
2024-04-02 09:43:11 +0200

    Solve ex 2.7

--- 02.control-structures/2.7/app.js
+++ 02.control-structures/2.7/app.js
@@ -1,16 +1,14 @@
 //Make a program that ask the user to enter a number named n. Then ask him n time to enter a new number. At the end display the sum of all the numbers collected this way.
 //Example: If the user enters 3 for n then 1, 2 and 3, the program should display 6.
 
-let n = new Number(prompt('Please pick a number.'));
-let arrayTotal = [];
+const n = new Number(prompt('Please pick a number.'));
+let sum;
 
 for (i = 1; i <= n; i++) {
-  const value = new Number(Prompt("Please enter another number"))
-  arrayTotal.push(value)
+  const value = new Number(Prompt("Please enter another number."))
+  sum += value
 }
 
-let sum = arrayTotal(total, nextInArray) {
-    total + nextInArray 
-}
+
 
 console.log(`Congratulations, you gave us numbers that, once added, totaled ${sum}! Such a huge number!`);
