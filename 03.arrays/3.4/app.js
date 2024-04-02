0cc255097f923acacfbe93d376c3d9e0105b0936
Julie Wustefeld <Urubaean>
2024-04-02 11:23:34 +0200

    solve ex 3.4

--- 03.arrays/3.4/app.js
+++ 03.arrays/3.4/app.js
@@ -1,3 +1,8 @@
-(() => {
-    // YOUR CODE HERE
-})();
+//Write a program that will display the minimum and maximum elements of a given array.
+
+const randomD20Array = [9, 4, 2, 7, 3] //this adventurer has shit luck and i wouldn't want to be in their party
+const min = Math.min(...randomD20Array);
+const max = Math.max(...randomD20Array);
+
+console.log(min);
+console.log(max);
