b0262e653604fefb094d820faa46f1090b6fd1ac
Julie Wustefeld <Urubaean>
2024-04-02 15:35:16 +0200

    add documentation to function in ex 4.2

--- 04.functions/4.2/app.js
+++ 04.functions/4.2/app.js
@@ -4,9 +4,9 @@ Write a documentation for the rand10() function.
 
 You will have to search on Google how to generate random numbers in JavaScript for this exercise.*/
 
-const rand10 = () => {
-    let randomNumber = Math.floor(Math.random() * 10 + 1);
-    console.log(randomNumber);
+const rand10 = () => { //function shorthand
+    let randomNumber = Math.floor(Math.random() * 10 + 1); //generate a number between 0 and 1, add one
+    console.log(randomNumber); //logs the random number into the console
 }
 
 rand10();
