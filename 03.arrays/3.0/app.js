b4f336029619cc1a03db5bef0bfa51bbf45a0fe4
Julie Wustefeld <Urubaean>
2024-04-02 10:00:50 +0200

    Solve ex 3.0

--- 03.arrays/3.0/app.js
+++ 03.arrays/3.0/app.js
@@ -1,3 +1,7 @@
-(() => {
-    // YOUR CODE HERE
-})();
+// Create an array that contains every learner from the class (you can find the complete list on the Discord server). Display each name in the console using a for ... of ... loop.
+
+const classmates = ["Adrien B.", "Adrien C.", "Angel", "Arnaud", "Caroline", "Denis", "Dorian", "Dylan", "dzheylyan", "Giovanni", "Isabelle", "Jessica", "Jordan", "Julie", "Justine F.", "Justine L.", "Iliess", "Ludovic", "Lyne", "Manu", "Maryann", "Mohamed", "Natalia", "Nathanael", "Stacy", "Tom", "Youris", "Zahra"];
+
+for (let x of classmates) {
+    console.log(x);
+};
