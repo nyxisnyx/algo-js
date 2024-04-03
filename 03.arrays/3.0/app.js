2d40805bae6afae8b6e2a3c4c8abb2428aa48a3d
JulieWustefeld <juliewustefeld@gmail.com>
2024-04-03 12:23:37 +0200

    fix array

--- 03.arrays/3.0/app.js
+++ 03.arrays/3.0/app.js
@@ -1,6 +1,6 @@
 // Create an array that contains every learner from the class (you can find the complete list on the Discord server). Display each name in the console using a for ... of ... loop.
 
-const classmates = ["Adrien B.", "Adrien C.", "Angel", "Arnaud", "Caroline", "Denis", "Dorian", "Dylan", "dzheylyan", "Giovanni", "Isabelle", "Jessica", "Jordan", "Julie", "Justine F.", "Justine L.", "Iliess", "Ludovic", "Lyne", "Manu", "Maryann", "Mohamed", "Natalia", "Nathanael", "Stacy", "Tom", "Youris", "Zahra"];
+const classmates = ["Adrien B.", "Adrien C.", "Angel", "Arnaud", "Caroline", "Denis", "Dorian", "Dylan", "Dzheylyan", "Giovanni", "Isabelle", "Jessica", "Jordan", "Julie", "Justine F.", "Justine L.", "Iliess", "Ludovic", "Lyne", "Manu", "Maryann", "Mohamed", "Natalia", "Nathanael", "Stacy", "Tom", "Youris", "Zahra"];
 
 for (let x of classmates) {
     console.log(x);
