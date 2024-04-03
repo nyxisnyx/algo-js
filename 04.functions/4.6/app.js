cfa6b237c95028fcbab5d6edeb3b0f4c68123305
JulieWustefeld <juliewustefeld@gmail.com>
2024-04-03 13:51:44 +0200

    solve ex 4.6

--- 04.functions/4.6/app.js
+++ 04.functions/4.6/app.js
@@ -1,3 +1,19 @@
-(() => {
-    // YOUR CODE HERE
-})();
+/*Create a function factorial(a) that returns the factorial of a number.
+
+That function must be recursive.*/
+let n = 4;
+
+function factorial(n){
+    if(n < 0){
+        return "Negative numbers don't have a factorial."
+    }
+
+    if(n == 0 || n == 1){
+        return 1;
+
+    }else{
+        return n * factorial(n-1);
+    }
+}
+
+console.log(`The factorial of ${n} is ${factorial(n)}.`);
